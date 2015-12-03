de63084f29625dd5367def7400421fa874f9f455
supervisor <josue0509@gmail.com>
2015-12-02 19:18:46 -0600

    Change script.js

--- sites/all/themes/UndergroundTheme/src-js/script.js
+++ sites/all/themes/UndergroundTheme/src-js/script.js
@@ -1,9 +1,7 @@
 // Manatí Base Theme JS functions
 (function($) {
 $(document).ready(function() {
-
 var btn_menu = $('.expanded');
-
 btn_menu.click(function(){
   $('.expanded > ul').toggle('expand-menu');
 });
