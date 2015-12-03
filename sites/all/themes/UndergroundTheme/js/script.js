8b4b437f57724e0ad0ef266098612404d7a42b4d
supervisor <josue0509@gmail.com>
2015-12-02 19:26:38 -0600

    Add script.js

---
+++ sites/all/themes/UndergroundTheme/js/script.js
@@ -0,0 +1,12 @@
+// Manatí Base Theme JS functions
+(function($) {
+$(document).ready(function() {
+var btn_menu = $('.expanded');
+btn_menu.click(function(){
+  $('.expanded > ul').toggle('expand-menu');
+});
+
+});
+
+})(jQuery);
+
