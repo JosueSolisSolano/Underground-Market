3e42a8da8a14f7139a20d64a3cd49248d0e1ec08
supervisor <josue0509@gmail.com>
2015-12-02 19:06:30 -0600

    Change main menu css and script.js

--- sites/all/themes/UndergroundTheme/src-js/script.js
+++ sites/all/themes/UndergroundTheme/src-js/script.js
@@ -1,13 +1,14 @@
 // Manatí Base Theme JS functions
 (function($) {
+$(document).ready(function() {
 
 var btn_menu = $('.expanded');
-console.log(btn_menu);
 
 btn_menu.click(function(){
-  alert("PUTA!!!");
-  var submenu = $('.expanded > ul');
   $('.expanded > ul').toggle('expand-menu');
 });
 
+});
+
 })(jQuery);
+
